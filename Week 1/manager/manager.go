f4084d489105a9ad9254dc5ce23d264f65de95fa
AKD2028 <akd.tg2024@gmail.com>
2026-01-27 20:09:26 +0530

    Cleaned the Manager

--- Week 1/manager/manager.go
+++ Week 1/manager/manager.go
@@ -9,21 +9,9 @@ import (
 	"week1/worker"
 )
 
-type Result struct {
-	FileSize       int64
-	RangeSupported bool
-}
-
-type Chunk struct {
-	ID    int
-	Start int64
-	End   int64
-}
-
 func Manager(url string, numChunks int) error {
-
-	result, err := probe.Probe(url)
-	fmt.Printf(" %s ", url)
+	//Probing the server
+	result, err := probe.Probe(url) //{FileSize,RangeSupported},err
 	if err != nil {
 		return fmt.Errorf("Error probing the url : %w", err)
 	}
@@ -31,19 +19,24 @@ func Manager(url string, numChunks int) error {
 		numChunks = 1
 	}
 
-	chunks := chunk.CreateChunks(result.FileSize, numChunks) // []Chunk
-	fmt.Println("✅ Chunks created:", len(chunks))
+	//Getting the chunks
+	chunks := chunk.CreateChunks(result.FileSize, numChunks) // []Chunk(ID,Start,End)
+	fmt.Println("Chunks created:", len(chunks))
+
+	//Getting paths
 	paths, err := paths.PathBuild(numChunks, url) // retuns []string,err
 	if err != nil {
 		return fmt.Errorf("Error getting the paths : %w", err)
 	}
 
+	//Calling the workers
 	wg := sync.WaitGroup{}
 	for i := 0; i < numChunks; i++ {
 		wg.Add(1)
 		go worker.Worker(url, chunks[i], paths[i], &wg) //passed st,end,pathToWrite,waitGroup
 	}
 
+	//Waiting for the workers
 	wg.Wait()
 
 	return nil
