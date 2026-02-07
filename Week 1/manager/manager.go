0cd2532a9a5d7da54eb8cbda2a304a8eaeb1f327
AKD2028 <akd.tg2024@gmail.com>
2026-02-07 18:54:25 +0530

    Internet Disruption Feature Update

--- Week 1/manager/manager.go
+++ Week 1/manager/manager.go
@@ -2,14 +2,16 @@ package manager
 
 import (
 	"fmt"
+	"net/http"
 	"sync"
+	"time"
 	"week1/chunk"
+	"week1/input"
 	"week1/merger"
 	"week1/paths"
 	"week1/probe"
 	"week1/progress"
 	"week1/worker"
-	"week1/input"
 )
 
 func Manager(url string, numChunks int) error {
@@ -46,30 +48,55 @@ func Manager(url string, numChunks int) error {
 	//Calling the workers
 	wg := sync.WaitGroup{}
 	Ctrl := worker.Controller{
-		PauseFlag: false,
+		PauseFlag:    false,
 		PauseChannel: nil,
-		CancelFlag: false,
+		CancelFlag:   false,
 	}
-	for i := 0; i < numChunks; i++ {
-		wg.Add(1)
-		go worker.Worker(url, chunks[i], partPaths[i],tracker, &wg,&Ctrl) //passed st,end,pathToWrite,waitGroup
+
+	wg2 := sync.WaitGroup{}
+	wg2.Add(1)
+	go func() {
+		for tracker.TotalDone < tracker.TotalSize {
+
+		//resuming logic
+		time.Sleep(time.Second)
+		fmt.Println("Trying connection again")
+		req, _ := http.NewRequest("HEAD", url, nil) //blocks without internet
+		client := &http.Client{}
+		_, err := client.Do(req)
+		fmt.Println("Made head request")
+		if err != nil {
+			continue
+		}
+		fmt.Println("Connection is back")
+		//
+
+		for i, chunk := range chunks {
+			chunk.Start += tracker.ChunkDone[i]
+		}
+		for i := 0; i < numChunks; i++ {
+			wg.Add(1)
+			go worker.Worker(url, chunks[i], partPaths[i], tracker, &wg, &Ctrl) //passed st,end,pathToWrite,waitGroup
+		}
+		wg.Wait()
+		fmt.Println("The workers have returned")
 	}
+	}()
 
 	go input.GetTerminalInput(&Ctrl)
 
 	//Waiting for the workers
 	wg.Wait()
-
+	wg2.Wait()
 
 	werr := merger.MergeChunks(partPaths, url)
 	if werr != nil {
 		return err
 	}
 
-	Ctrl.CancelFlag=false
-	Ctrl.PauseChannel=nil
-	Ctrl.PauseFlag=false
-
+	Ctrl.CancelFlag = false
+	Ctrl.PauseChannel = nil
+	Ctrl.PauseFlag = false
 
 	return nil
 
--- Week 1/worker/worker.go
+++ Week 1/worker/worker.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"sync"
+	"time"
 	"week1/chunk"
 	"week1/progress"
 	"week1/writer"
@@ -15,6 +16,13 @@ type Controller struct{
 	PauseChannel chan struct{}
 	CancelFlag bool
 }
+
+
+type ReadResult struct{
+	N int
+	E error
+}
+
 
 func Worker(url string, ch chunk.Chunk, filepath string, tracker *progress.Tracker,wg *sync.WaitGroup,Ctrl *Controller){
 	defer (*wg).Done()
@@ -39,19 +47,45 @@ func Worker(url string, ch chunk.Chunk, filepath string, tracker *progress.Track
 	buf := make([]byte, 32*1024) // 32KB buffer
 
 	for {
-		
 		if (Ctrl.PauseFlag){
 			fmt.Printf("Downloading paused for chunk %d\n",ch.ID)
 			<-Ctrl.PauseChannel
 		}
-
 		if (Ctrl.CancelFlag){
 			fmt.Printf("Downloading cancelled for chunk %d\n", ch.ID)
 			return
 		}
 
-		n, err := resp.Body.Read(buf)
+		//
+		readChan := make(chan ReadResult)
+		var currReadResult ReadResult 
+		go func(){
+			n, err := resp.Body.Read(buf)
+			result := ReadResult{
+				N : n,
+				E : err,
+			}
+			readChan<-result
+		}()
 
+		select {
+		case currReadResult=<-readChan :
+			//got the bytes
+		case <-time.After(10*time.Second):
+			fmt.Println("Connection disrupted")
+			return
+		}
+		//
+
+		// if (Ctrl.PauseFlag){
+		// 	fmt.Printf("Downloading paused for chunk %d\n",ch.ID)
+		// 	<-Ctrl.PauseChannel
+		// }
+
+		
+
+		n:= currReadResult.N
+		err =currReadResult.E
 		if n > 0 {
 			writer.Write(buf[:n], filepath)
 
